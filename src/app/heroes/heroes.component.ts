112aacc674f67a177703f2e5bb4541c5a7eda4c8
Vaibhav Sharma <vaibhav.sharma@athenasoft.in>
2019-03-08 21:09:53 +0530

    changes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero.model';
+import { HEROES } from './mockheroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  public hero:Hero = new Hero();
+  public selectedhero:Hero = new Hero();
+  public heroes:Hero[] = [];
+
   constructor() { 
-    this.hero.name="Windstorm";
-    this.hero.id = 2;
+    this.selectedhero.name="Windstorm";
+    this.selectedhero.id = 2;
   }
 
   ngOnInit() {
